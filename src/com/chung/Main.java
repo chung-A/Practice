a1ad183d1709ab403e44ca0f6b7f320e440a571a
chung-A <ydh9517@naver.com>
2021-03-04 01:42:55 +0900

    백 1181 단어 정렬
    https://www.acmicpc.net/problem/1181

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,7 +3,8 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
 
@@ -12,13 +13,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        int x = Integer.parseInt(st.nextToken());
-        int y = Integer.parseInt(st.nextToken());
-        int w = Integer.parseInt(st.nextToken());
-        int h = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
 
-        int ans = Math.min(Math.abs(x - w), x);
-        int ans2 = Math.min(Math.abs(y - h), y);
-        System.out.println(Math.min(ans, ans2));
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            list.add(br.readLine());
+        }
+
+        List<String> collect = list.stream().sorted((s1, s2) -> {
+            if (s1.length() > s2.length()) {
+                return 1;
+            } else if (s1.length() < s2.length()) {
+                return -1;
+            } else {
+                for (int i = 0; i < s1.length(); i++) {
+                    if (s1.charAt(i) < s2.charAt(i)) {
+                        return -1;
+                    } else if (s1.charAt(i) > s2.charAt(i)) {
+                        return 1;
+                    }
+                }
+                return 0;
+            }
+        }).collect(Collectors.toList());
+
+        StringBuilder stb = new StringBuilder();
+
+        stb.append(collect.get(0)).append('\n');
+        for (int i = 1; i < n; i++) {
+            if (!collect.get(i).equals(collect.get(i - 1))) {
+                stb.append(collect.get(i)).append('\n');
+            }
+        }
+        System.out.println(stb);
     }
 }
