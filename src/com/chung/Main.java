54a2ac985da810c00d8f784aaf7dc8712677ea5a
chung-A <ydh9517@naver.com>
2021-03-04 02:34:50 +0900

    백 1436 영화감독 숌

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,8 +3,7 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -14,37 +13,14 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int n = Integer.parseInt(st.nextToken());
-
-        List<String> list = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            list.add(br.readLine());
-        }
-
-        List<String> collect = list.stream().sorted((s1, s2) -> {
-            if (s1.length() > s2.length()) {
-                return 1;
-            } else if (s1.length() < s2.length()) {
-                return -1;
-            } else {
-                for (int i = 0; i < s1.length(); i++) {
-                    if (s1.charAt(i) < s2.charAt(i)) {
-                        return -1;
-                    } else if (s1.charAt(i) > s2.charAt(i)) {
-                        return 1;
-                    }
-                }
-                return 0;
-            }
-        }).collect(Collectors.toList());
-
-        StringBuilder stb = new StringBuilder();
-
-        stb.append(collect.get(0)).append('\n');
-        for (int i = 1; i < n; i++) {
-            if (!collect.get(i).equals(collect.get(i - 1))) {
-                stb.append(collect.get(i)).append('\n');
+        int number = 666;
+        int count = 1;
+        while (n != count) {
+            number++;
+            if (Integer.toString(number).contains("666")) {
+                count ++;
             }
         }
-        System.out.println(stb);
+        System.out.println(number);
     }
 }
