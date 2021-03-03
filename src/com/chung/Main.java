12fbef875efec8b2a16339947db22b0ea2746fc0
chung-A <ydh9517@naver.com>
2021-03-03 18:09:45 +0900

    백 1018 체스판 다시 칠하기

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -1,19 +1,68 @@
 package com.chung;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.util.Scanner;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         //input
-        Scanner scanner = new Scanner(System.in);
-        int a = scanner.nextInt();
-        int b = scanner.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int Y = Integer.parseInt(st.nextToken());
+        int X = Integer.parseInt(st.nextToken());
 
-        int ra = (a / 100) + ((a % 100) / 10) * 10 + (a % 10) * 100;
-        int rb = (b / 100) + ((b % 100) / 10) * 10 + (b % 10) * 100;
+        //init
+        char[][] map = new char[Y][X];
+        for (int i = 0; i < map.length; i++) {
+            String word = br.readLine();
+            for (int j = 0; j < map[i].length; j++) {
+                map[i][j] = word.charAt(j);
+            }
+        }
 
-        System.out.println(Math.max(ra, rb));
+        //do
+        int count = 64;
+        for (int i = 0; i < Y - 7; i++) {
+            for (int j = 0; j < X - 7; j++) {
+                int compare = recursive(map, j, i, X, Y);
+
+                count = Math.min(count, compare);
+            }
+        }
+
+        System.out.println(count);
+    }
+
+    public static int recursive(char[][] map, int x, int y, int X, int Y) {
+        int count = 0;
+
+        //count
+        char next = map[y][x];
+        count = getDrawCount(map, x, y, next, count);
+
+        int compare = 64 - count;
+        count = Math.min(count, compare);
+
+        return count;
+    }
+
+    private static int getDrawCount(char[][] map, int x, int y, char next, int count) {
+        for (int i = y; i < y + 8; i++) {
+            for (int j = x; j < x + 8; j++) {
+                if (next != map[i][j]) {
+                    count++;
+                }
+                next = reverse(next);
+            }
+            next = reverse(next);
+        }
+        return count;
+    }
+
+    public static char reverse(char c) {
+        return (c == 'B') ? 'W' : 'B';
     }
 }
