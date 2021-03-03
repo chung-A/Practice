eba4539f2115512f0886c9ca434a7a997e2544cd
chung-A <ydh9517@naver.com>
2021-03-03 12:04:31 +0900

    백 9663 N-Queen

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -8,16 +8,42 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //input
         Scanner scanner = new Scanner(System.in);
-        int[] cnt = new int[6];
-        int[] full = new int[]{1, 1, 2, 2, 2, 8};
-        for (int i = 0; i < 6; i++) {
-            cnt[i] = scanner.nextInt();
+        int n = scanner.nextInt();
+        int[] cols = new int[n + 1];
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            cols[1] = i;
+            answer += queens(n, cols, 1);
         }
 
-        //logic
-        for (int i = 0; i < full.length; i++) {
-            cnt[i] = full[i] - cnt[i];
-            System.out.println(cnt[i]);
+        System.out.println(answer);
+    }
+
+    public static int queens(int n, int[] cols, int level) {
+        if (!isPromised(cols, level)) {
+            return 0;
+        } else if (level == n) {
+            return 1;
+        } else {
+            int sum = 0;
+            for (int i = 1; i <= n; i++) {
+                cols[level + 1] = i;
+                sum += queens(n, cols, level + 1);
+            }
+            return sum;
         }
+    }
+
+    public static boolean isPromised(int[] cols, int level) {
+        for (int i = 1; i < level; i++) {
+            if (cols[i] == cols[level]) {
+                return false;
+            }
+            else if (level - i == Math.abs(cols[level] - cols[i])) {
+                return false;
+            }
+        }
+        return true;
     }
 }
