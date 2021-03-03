fba549e6170022b1a6bea4ed30a884382e22fc39
chung-A <ydh9517@naver.com>
2021-03-04 02:59:44 +0900

    백 1654 랜선자르기

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,6 +3,7 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -12,15 +13,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int k = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        int number = 666;
-        int count = 1;
-        while (n != count) {
-            number++;
-            if (Integer.toString(number).contains("666")) {
-                count ++;
+
+        int[] lens = new int[k];
+        for (int i = 0; i < k; i++) {
+            st = new StringTokenizer(br.readLine());
+            lens[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(lens);
+
+        long left = 1;
+        long right = lens[lens.length - 1];
+        while (left <= right) {
+            long middle = (left + right) / 2;
+            long sum = 0;
+            for (int i = 0; i < k; i++) {
+                sum += lens[i] / middle;
+            }
+
+            if (sum >= n) {
+                left = middle + 1;
+            } else {
+                right = middle - 1;
             }
         }
-        System.out.println(number);
+
+        System.out.println(right);
     }
 }
