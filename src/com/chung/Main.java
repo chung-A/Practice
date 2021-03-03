12df552ac2487637189c4f02403f899c23bd7749
chung-A <ydh9517@naver.com>
2021-03-03 14:15:35 +0900

    백 2914 저작권

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -8,19 +8,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //input
         Scanner scanner = new Scanner(System.in);
-        int[] result = new int[5];
+        int cnt = scanner.nextInt();
+        int avg = scanner.nextInt();
 
-        int p = scanner.nextInt();
-        int area = scanner.nextInt();
-        int cnt = p * area;
-
-        for (int i = 0; i < 5; i++) {
-            int temp = scanner.nextInt();
-            result[i] = temp - cnt;
-        }
-
-        for (int i : result) {
-            System.out.println(i);
-        }
+        int i = (avg - 1) * cnt + 1;
+        System.out.println(i);
     }
 }
