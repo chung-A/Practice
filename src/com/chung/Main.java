0778fa87a08cf639d85ad543bed5de2a7a229852
chung-A <ydh9517@naver.com>
2021-03-03 12:35:15 +0900

    백 2338 긴자리 계산

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -1,6 +1,7 @@
 package com.chung;
 
 import java.io.IOException;
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class Main {
@@ -8,42 +9,14 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //input
         Scanner scanner = new Scanner(System.in);
-        int n = scanner.nextInt();
-        int[] cols = new int[n + 1];
+        String a = scanner.next();
+        String b = scanner.next();
 
-        int answer = 0;
-        for (int i = 1; i <= n; i++) {
-            cols[1] = i;
-            answer += queens(n, cols, 1);
-        }
+        BigInteger bigIntegerA = new BigInteger(a);
+        BigInteger bigIntegerB = new BigInteger(b);
 
-        System.out.println(answer);
-    }
-
-    public static int queens(int n, int[] cols, int level) {
-        if (!isPromised(cols, level)) {
-            return 0;
-        } else if (level == n) {
-            return 1;
-        } else {
-            int sum = 0;
-            for (int i = 1; i <= n; i++) {
-                cols[level + 1] = i;
-                sum += queens(n, cols, level + 1);
-            }
-            return sum;
-        }
-    }
-
-    public static boolean isPromised(int[] cols, int level) {
-        for (int i = 1; i < level; i++) {
-            if (cols[i] == cols[level]) {
-                return false;
-            }
-            else if (level - i == Math.abs(cols[level] - cols[i])) {
-                return false;
-            }
-        }
-        return true;
+        System.out.println(bigIntegerA.add(bigIntegerB));
+        System.out.println(bigIntegerA.subtract(bigIntegerB));
+        System.out.println(bigIntegerA.multiply(bigIntegerB));
     }
 }
