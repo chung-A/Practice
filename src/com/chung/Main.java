2f90e161f9e37c810d6b5e8d7a1a3620e55c9d8b
chung-A <ydh9517@naver.com>
2021-03-03 14:51:33 +0900

    백 1157 단어 공부

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -1,27 +1,48 @@
 package com.chung;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         //input
         Scanner scanner = new Scanner(System.in);
-        StringBuilder stb = new StringBuilder();
-        int t = scanner.nextInt();
+        HashMap<Character, Integer> map = new HashMap<>();
+        String word = scanner.next();
 
-        for (int i = 0; i < t; i++) {
-            int r = scanner.nextInt();
-            String s = scanner.next();
-
-            for (int j = 0; j < s.length(); j++) {
-                for (int k = 0; k < r; k++) {
-                    stb.append(s.charAt(j));
-                }
+        for (int i = 0; i < word.length(); i++) {
+            int index = word.charAt(i);
+            if (index > 91) {
+                index -= 32;
             }
-            System.out.println(stb.toString());
-            stb.setLength(0);
+
+            char character = (char) index;
+            if (!map.containsKey(character)) {
+                map.put(character, 1);
+            }
+            else{
+                map.replace(character, map.get(character) + 1);
+            }
         }
+
+        char targetKey=' ';
+        int max = 0;
+        List<Integer> collect = map.values().stream().sorted((c1, c2) -> c2 - c1).collect(Collectors.toList());
+        if (collect.size() > 1 && collect.get(0).equals(collect.get(1))) {
+            System.out.println("?");
+            return;
+        }
+
+        for (Character character : map.keySet()) {
+            if (map.get(character) > max) {
+                targetKey = character;
+                max = map.get(character);
+            }
+        }
+        System.out.println(targetKey);
     }
 }
