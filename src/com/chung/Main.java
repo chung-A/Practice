0a2a52558d5ceab28a563a317b762bda4bf5f6af
chung-A <ydh9517@naver.com>
2021-03-04 18:52:43 +0900

    백준 14852번 타일 채우기3

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,40 +3,29 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 public class Main {
 
-    static long[] arr = new long[31];
+    static long[][] arr = new long[1000001][2];
 
     public static void main(String[] args) throws IOException {
         //input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        if (n % 2 == 1) {
-            System.out.println(0);
-            return;
-        }
-
-        Arrays.fill(arr, -1);
 
         System.out.println(dynamic(n));
     }
 
     static long dynamic(int idx) {
-        if (idx == 2) return 3;
-        else if(idx==0) return 1;
-        else if (arr[idx] != -1) {
-            return arr[idx];
-        } else {
-            long a = 3 * dynamic(idx - 2);
+        arr[0][0] = 0;
+        arr[1][0] = 2;
+        arr[2][0] = 7;
+        arr[2][1] = 1;
 
-            long sum = 0;
-            for (int i = idx - 4; i >= 0; i -= 2) {
-                sum += 2 * dynamic(i);
-            }
-
-            return arr[idx] = (a + sum);
+        for (int i = 3; i <= idx; i++) {
+            arr[i][1] = (arr[i - 1][1] + arr[i - 3][0]) % 1000000007;
+            arr[i][0] = (3 * arr[i - 2][0] + 2 * arr[i - 1][0] + 2 * arr[i][1]) % 1000000007;
         }
+        return arr[idx][0];
     }
 }
