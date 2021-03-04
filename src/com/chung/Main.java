6fb376aa570301b5ccced2973193c796a58ed4bd
chung-A <ydh9517@naver.com>
2021-03-04 12:10:37 +0900

    백 1259번 팰린드롬수

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,7 +3,8 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -11,35 +12,29 @@ public class Main {
     public static void main(String[] args) throws IOException {
         //input
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
-        int k = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-
-        int[] lens = new int[k];
-        for (int i = 0; i < k; i++) {
+        List<String> list = new ArrayList<>();
+        while (true) {
             st = new StringTokenizer(br.readLine());
-            lens[i] = Integer.parseInt(st.nextToken());
+            String k = st.nextToken();
+
+            if (k.equals("0")) break;
+
+            list.add(k);
         }
 
-        Arrays.sort(lens);
-
-        long left = 1;
-        long right = lens[lens.length - 1];
-        while (left <= right) {
-            long middle = (left + right) / 2;
-            long sum = 0;
-            for (int i = 0; i < k; i++) {
-                sum += lens[i] / middle;
+        for (String s : list) {
+            String rst = "yes";
+            int length = s.length() / 2;
+            for (int i = 0; i < length; i++) {
+                if (s.charAt(i) != s.charAt(s.length() - 1 - i)) {
+                    rst = "no";
+                    break;
+                }
             }
 
-            if (sum >= n) {
-                left = middle + 1;
-            } else {
-                right = middle - 1;
-            }
+            System.out.println(rst);
         }
-
-        System.out.println(right);
     }
 }
