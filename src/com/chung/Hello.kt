d10b8cc0a7e9e678d969ae8c69271328744c43b8
chung-A <ydh9517@naver.com>
2021-03-06 00:45:59 +0900

    Add kotiln

---
+++ src/com/chung/Hello.kt
@@ -0,0 +1,9 @@
+package com.chung
+
+fun main(args: Array<String>) {
+    println("hello")
+}
+
+fun call():String {
+    return "i am kotlin"
+}
--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,41 +3,27 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.time.LocalDateTime;
-import java.util.Arrays;
-import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        //input
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println(60*24);
+
+//        //input
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 //        int n = Integer.parseInt(br.readLine());
-        int n = 1000000;
-        Random random = new Random();
-
-
-        int[] data = new int[n];
-        int[] data2 = new int[n];
-        for (int i = 0; i < n; i++) {
+//
+//        int[] data = new int[n];
+//        for (int i = 0; i < n; i++) {
 //            data[i] = Integer.parseInt(br.readLine());
-            int ran = random.nextInt(10000);
-            data[i] = ran;
-            data2[i] = ran;
-        }
-        br.close();
-
-        long startTime = System.currentTimeMillis();
-        quickSort(data, 0, data.length - 1);
-        long endTime = System.currentTimeMillis();
-        System.out.println("퀵정렬 걸린시간: " + (endTime - startTime) * 0.001);
-
-        startTime = System.currentTimeMillis();
-        normalSort(data2);
-        endTime = System.currentTimeMillis();
-        System.out.println("선택정렬 걸린시간: " + (endTime - startTime) * 0.001);
-
-//        System.out.println(Arrays.toString(data));
+//        }
+//        br.close();
+//
+//        quickSort(data, 0, data.length - 1);
+//
+//        for (int num : data) {
+//            System.out.println(num);
+//        }
     }
 
     static void quickSort(int[] data, int l, int r) {
@@ -45,8 +31,8 @@ public class Main {
         pivot = data[(l + r) / 2];
 
         do {
-            while (data[left] > pivot) left++;
-            while (data[right] < pivot) right--;
+            while (data[left] < pivot) left++;
+            while (data[right] > pivot) right--;
             if (left <= right) {
                 temp = data[left];
                 data[left] = data[right];
@@ -58,24 +44,5 @@ public class Main {
 
         if (l < right) quickSort(data, l, right);
         if (r > left) quickSort(data, left, r);
-    }
-
-    static void normalSort(int[] data) {
-        int idx, temp;
-
-        for (int i = 0; i < data.length; i++) {
-            idx = i;
-            for (int j = i + 1; j < data.length; j++) {
-                if (data[idx] > data[j]) {
-                    idx = j;
-                }
-            }
-
-            if (idx != i) {
-                temp = data[idx];
-                data[idx] = data[i];
-                data[i] = temp;
-            }
-        }
     }
 }
