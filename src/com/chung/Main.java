cc8df7bda1519148eccbab026daefa339f93bf28
chung-A <ydh9517@naver.com>
2021-04-30 15:41:26 +0900

    Programmers 아이디만들기

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -9,76 +9,112 @@ import java.util.Queue;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        int MAX = 200000;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] strings = br.readLine().split(" ");
+        Programmers programmers = new Programmers();
+        String solution = programmers.solution("...!@BaT#*..y.abc-_.defghijklm.");
+        System.out.println("solution = " + solution);
+    }
 
-        boolean[] isVisited = new boolean[MAX];
-        int[] count = new int[MAX];
-        int n = Integer.parseInt(strings[0]);
-        int k = Integer.parseInt(strings[1]);
-
-        Queue<Integer> queue = new LinkedList<>();
-        queue.add(n);
-        isVisited[n] = true;
-        count[n] = 0;
-
-        while (!queue.isEmpty()) {
-            int now = queue.remove();
-
-            //x+1
-            if (now + 1 < MAX) {
-                if (!isVisited[now + 1]) {
-                    queue.add(now + 1);
-                    isVisited[now + 1] = true;
-                    count[now + 1] = count[now] + 1;
+    // sum 은 지금까지의 합
+    static long go(int start, int end, int n, long target, long[] data, long sum, long answer) {
+        if (end <= n) {
+            if (sum < target) {
+                answer = go(start, end + 1, n, target, data, sum + data[end], answer);
+            } else {
+                if (sum == target) {
+                    answer += 1;
                 }
-            }
-
-            //x-1
-            if (now - 1 >= 0) {
-                if (!isVisited[now - 1]) {
-                    queue.add(now - 1);
-                    isVisited[now - 1] = true;
-                    count[now - 1] = count[now] + 1;
-                }
-            }
-
-            // x*2
-            if (now * 2 < MAX) {
-                if (!isVisited[now * 2]) {
-                    queue.add(2 * now);
-                    isVisited[now * 2] = true;
-                    count[2 * now] = count[now] + 1;
-                }
+                answer = go(start + 1, end, n, target, data, sum - data[start], answer);
             }
         }
-
-        System.out.println(count[k]);
+        return answer;
     }
 
     /***************************************************************
      * Programmers
      **************************************************************/
     public static class Programmers {
-        int[] answer = new int[2];
-        public int[] solution(int brown, int yellow) {
 
-            for (int i = 3; i <= 5000; i++) {
-                for (int j = 1; j <= i; j++) {
-                    if (check(brown, yellow, i, j)) {
-                        answer[0] = i;
-                        answer[1] = j;
-                    }
-                }
-            }
+        public String solution(String new_id) {
+            String recommend = step1(new_id);
+            recommend = step2(recommend);
+            recommend = step3(recommend);
+            recommend = step4(recommend);
+            recommend = step5(recommend);
+            recommend = step6(recommend);
+            recommend = step7(recommend);
 
-            return answer;
+            return recommend;
         }
 
-        boolean check(int brown, int yellow, int row, int col) {
-            return row >= col && (row * col) - ((row - 2) * (col - 2)) == brown
-                    && (row - 2) * (col - 2) == yellow;
+
+         String step1(String id) {
+            return id.toLowerCase();
+        }
+
+         String step2(String id) {
+             return id.replaceAll("[^0-9a-zA-Z\\.\\-\\_]", "");
+//             StringBuilder stb = new StringBuilder();
+//             for (int i = 0; i < id.length(); i++) {
+//                 char c = id.charAt(i);
+//
+//                 if (c == '.' || c == '_' || c == '-' || (c >= '0' && c <= '9') || (c >= 'a' && c <= 'z')) {
+//                     stb.append(c);
+//                 }
+//             }
+//             return stb.toString();
+        }
+
+         String step3(String id) {
+            id.matches(".[0-9]*");
+            return id.replaceAll("[.]+", ".");
+        }
+
+         String step4(String id) {
+             id = id.replaceAll("^.", "");
+             id = id.replaceAll(".$", "");
+             return id;
+
+//            if (id.length() > 0 && id.charAt(0) == '.') {
+//                id = id.substring(1);
+//            }
+//
+//            if (id.length() > 0 && id.charAt(id.length()-1) == '.') {
+//                id = id.substring(0, id.length() - 1);
+//            }
+//            return id;
+        }
+
+         String step5(String id) {
+            if (id == null || "".equals(id)) {
+                id = "a";
+            }
+            return id;
+        }
+
+         String step6(String id) {
+            if (id.length() >= 16) {
+                StringBuilder stb = new StringBuilder();
+                for (int i = 0; i < 15; i++) {
+                    if(i==14&&id.charAt(14)=='.') continue;
+                    stb.append(id.charAt(i));
+                }
+                id = stb.toString();
+            }
+            return id;
+        }
+
+         String step7(String id) {
+            if (id.length() <= 2) {
+                char lastChar = id.charAt(id.length() - 1);
+                StringBuilder idBuilder = new StringBuilder(id);
+
+                for (int i = 0; i < 3 - id.length(); i++) {
+                    idBuilder.append(lastChar);
+                }
+                id = idBuilder.toString();
+            }
+            return id;
         }
     }
 }
