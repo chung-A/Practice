56a906f39c50119d3127765feb742daca7d84b5c
chung-A <ydh9517@naver.com>
2021-05-02 11:31:07 +0900

    백 14470번 전자레인지

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,31 +3,33 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Programmers programmers = new Programmers();
-        int solution = programmers.solution("10D10S#10S*");
-        System.out.println("solution = " + solution);
+        int a = Integer.parseInt(br.readLine());
+        int b = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
+        int d = Integer.parseInt(br.readLine());
+        int e = Integer.parseInt(br.readLine());
+
+        System.out.println(go(a, b, c, d, e));
     }
 
-    // sum 은 지금까지의 합
-    static long go(int start, int end, int n, long target, long[] data, long sum, long answer) {
-        if (end <= n) {
-            if (sum < target) {
-                answer = go(start, end + 1, n, target, data, sum + data[end], answer);
-            } else {
-                if (sum == target) {
-                    answer += 1;
-                }
-                answer = go(start + 1, end, n, target, data, sum - data[start], answer);
-            }
+    static int go(int now, int target, int c, int d, int e) {
+        if (now == target) {
+            return 0;
         }
-        return answer;
+        else if (now < 0) {
+            return go(now + 1, target, c, d, e) + c;
+        }
+        else if(now==0){
+            return go(now + 1, target, c, d, e) + d + e;
+        }
+        else{
+            return go(now + 1, target, c, d, e) + e;
+        }
     }
 
     /***************************************************************
@@ -36,116 +38,7 @@ public class Main {
     public static class Programmers {
 
         public int solution(String dartResult) {
-            String[] data = new String[3];
-            int index=0;
-
-            //parsing
-            StringBuilder stb = new StringBuilder();
-
-            //10점 검사
-            int start = 1;
-            if(dartResult.charAt(0)=='1'){
-                if(dartResult.charAt(1)=='0'){
-                    stb.append("10");
-                    start = 2;
-                }
-            }
-
-            if (start == 1) {
-                stb.append(dartResult.charAt(0));
-            }
-
-            for (int i = start; i < dartResult.length(); i++) {
-                char c = dartResult.charAt(i);
-
-                //10점 검사
-                if(c=='1'){
-                    if(dartResult.charAt(i+1)=='0'){
-                        data[index] = stb.toString();
-                        stb.setLength(0);
-                        index++;
-
-                        stb.append("10");
-                        i++;
-                        continue;
-                    }
-                }
-
-                if (c >= '0' && c <= '9') {
-                    data[index] = stb.toString();
-                    stb.setLength(0);
-                    index++;
-                }
-
-                stb.append(c);
-
-                if (i <= dartResult.length() - 1) {
-                    data[index] = stb.toString();
-                }
-            }
-
-            //cal
-            int[] points = new int[3];
-            for (int i = 0; i < data.length; i++) {
-                cal(data[i], points, i);
-            }
-
-            int sum = 0;
-            for (int point : points) {
-                sum += point;
-            }
-
-            return sum;
-        }
-
-
-        //return sum
-        void cal(String data,int[] points,int index) {
-            //10점 검사
-            int areaIndex = 1;
-            int p = Integer.parseInt(data.charAt(0)+"");
-            if(data.charAt(0)=='1'){
-                if(data.charAt(1)=='0'){
-                    p = 10;
-                    areaIndex = 2;
-                }
-            }
-
-            char bonus = data.charAt(areaIndex);
-            switch (bonus) {
-                case 'S':
-                    break;
-                case 'D':
-                    p = p * p;
-                    break;
-                case 'T':
-                    p = p * p * p;
-                    break;
-            }
-
-            //option
-            if (areaIndex==1&&data.length() > 2) {
-                if (data.charAt(2) == '#') {
-                    p = p * -1;
-                }
-                else if (data.charAt(2) == '*') {
-                    p = p * 2;
-                    if(index>0){
-                        points[index - 1] = points[index - 1] * 2;
-                    }
-                }
-            } else if (areaIndex == 2 && data.length() > 3) {
-                if (data.charAt(3) == '#') {
-                    p = p * -1;
-                } else if (data.charAt(3) == '*') {
-                    p = p * 2;
-                    if (index > 0) {
-                        points[index - 1] = points[index - 1] * 2;
-                    }
-                }
-            }
-
-            points[index] = p;
+            return 0;
         }
 
         // 이 위부터
