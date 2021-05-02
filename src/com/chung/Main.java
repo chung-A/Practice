d6cb8990552cdd9cf2c52c41c6b8d556c36f1966
chung-A <ydh9517@naver.com>
2021-05-02 13:35:00 +0900

    백 1059번 좋은 구간

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,31 +3,62 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String data = br.readLine();
+        int l = Integer.parseInt(br.readLine());
+        int[] data = new int[l];
+        String[] strings = br.readLine().split(" ");
+        for (int i = 0; i < strings.length; i++) {
+            data[i] = Integer.parseInt(strings[i]);
+        }
+        data = Arrays.stream(data).sorted().toArray();
 
+        int n = Integer.parseInt(br.readLine());
+
+        int startPoint = -1;
+        int endPoint = 10000;
+        for (int number : data) {
+            if (number < n) {
+                startPoint = number;
+            } else if (number > n) {
+                if(endPoint > number) {
+                    endPoint = number;
+                }
+            } else {
+                System.out.println(0);
+                return;
+            }
+
+            if (startPoint != -1 && endPoint != 10000) {
+                break;
+            }
+        }
+
+        if (startPoint == -1) {
+            startPoint = 0;
+        }
+        if (endPoint == -1) {
+            endPoint = 1001;
+        }
+
+        //i는 시작점
         int answer = 0;
-        int index = 1;
-        if(data.length()>0) {
-            char std = data.charAt(0);
+        for (int i = startPoint + 1; i < endPoint; i++) {
+            if(i>n){
+                break;
+            }
 
-            while (index < data.length()) {
-                if (data.charAt(index) == std){
-                    index++;
+            //j 는 끝점
+            for (int j = n; j < endPoint; j++) {
+                if (i == j) {
                     continue;
                 }
-
-                answer++;
-                for (int i = index; i < data.length(); i++) {
-                    if (data.charAt(i) == std) {
-                        break;
-                    }
-                    index++;
-                }
+//                System.out.println("(i,j) = (" + i + "," + j + ")");
+                answer ++;
             }
         }
 
