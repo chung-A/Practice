084be9c3b2a9047f37aa0c393e211b7e07de36e2
chung-A <ydh9517@naver.com>
2021-05-02 12:27:28 +0900

    백 1439번 뒤집기

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -8,28 +8,30 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int a = Integer.parseInt(br.readLine());
-        int b = Integer.parseInt(br.readLine());
-        int c = Integer.parseInt(br.readLine());
-        int d = Integer.parseInt(br.readLine());
-        int e = Integer.parseInt(br.readLine());
+        String data = br.readLine();
 
-        System.out.println(go(a, b, c, d, e));
-    }
+        int answer = 0;
+        int index = 1;
+        if(data.length()>0) {
+            char std = data.charAt(0);
 
-    static int go(int now, int target, int c, int d, int e) {
-        if (now == target) {
-            return 0;
+            while (index < data.length()) {
+                if (data.charAt(index) == std){
+                    index++;
+                    continue;
+                }
+
+                answer++;
+                for (int i = index; i < data.length(); i++) {
+                    if (data.charAt(i) == std) {
+                        break;
+                    }
+                    index++;
+                }
+            }
         }
-        else if (now < 0) {
-            return go(now + 1, target, c, d, e) + c;
-        }
-        else if(now==0){
-            return go(now + 1, target, c, d, e) + d + e;
-        }
-        else{
-            return go(now + 1, target, c, d, e) + e;
-        }
+
+        System.out.println(answer);
     }
 
     /***************************************************************
