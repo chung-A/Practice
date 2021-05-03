76764d2079f4857b46267e534e8b2d47c0f698bc
chung-A <ydh9517@naver.com>
2021-05-03 20:15:13 +0900

    백 1929번 소수구하기

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -6,41 +6,35 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    static boolean[] isNotPrime = new boolean[1000001];
+
     public static void main(String[] args) throws IOException {
-        Programmers p = new Programmers();
-        String abcd = p.solution("ab");
-        System.out.println(abcd);
-
-/*        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] split = br.readLine().split(" ");
-        int a = Integer.parseInt(split[0]);
-        int b = Integer.parseInt(split[1]);
-        int c = Integer.parseInt(split[2]);
-        int n = Integer.parseInt(split[3]);
+        int m = Integer.parseInt(split[0]);
+        int n = Integer.parseInt(split[1]);
 
-        boolean findAnswer = false;
-        int length1 = 300;
-        int length2 = 300;
-        int length3 = 300;
-
-        for (int i = 0; i < length1; i++) {
-            for (int j = 0; j < length2; j++) {
-                for (int k = 0; k < length3; k++) {
-                    int total = a * i + b * j + c * k;
-
-                    if (total == n) {
-                        findAnswer = true;
-                        break;
-                    }
-                }
-
-                if(findAnswer) break;
+        for (int i = 2; i <= n; i++) {
+            if (!isNotPrime[i]) {
+                erase(i, n);
             }
-            if(findAnswer) break;
         }
 
-        System.out.println((findAnswer) ? 1 : 0);*/
+        for (int i = m; i <= n; i++) {
+            if (!isNotPrime[i] && i > 1) {
+                System.out.println(i);
+            }
+        }
     }
+
+    //number 를 제외한 수들을 지운다.
+    static void erase(int number, int max) {
+        int length = max / number;
+        for (int i = 2; i <= length; i++) {
+            isNotPrime[number * i] = true;
+        }
+    }
+
 
     /***************************************************************
      * Programmers
@@ -51,9 +45,9 @@ public class Main {
             String answer = "";
 
             if (s.length() % 2 == 0) {
-                answer += s.charAt(s.length() / 2 -1);
+                answer += s.charAt(s.length() / 2 - 1);
                 answer += s.charAt(s.length() / 2);
-            }else{
+            } else {
                 answer += s.charAt(s.length() / 2);
             }
             return answer;
