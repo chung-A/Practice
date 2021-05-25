08b56e7b771acf70620c6861562c75352924f90b
chung-A <ydh9517@naver.com>
2021-05-25 18:56:59 +0900

    백 14225번 부분수열의 합(비트마스크)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -4,109 +4,44 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    static boolean[] contains = new boolean[100000 * 20 + 1];
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
         String[] split = br.readLine().split(" ");
-        int n = Integer.parseInt(split[0]);
-        int s = Integer.parseInt(split[1]);
 
         int[] data = new int[n];
-        split = br.readLine().split(" ");
         for (int i = 0; i < n; i++) {
             data[i] = Integer.parseInt(split[i]);
         }
 
-        List<Integer> leftList = new ArrayList<>();
-        List<Integer> rightList = new ArrayList<>();
-
-        int left = n / 2;
-        int right = n - left;
-
-        //left
-        for (int i = 0; i < (1 << left); i++) {
+        //시작
+        for (int i = 1; i < 1 << n; i++) {
 
             int sum = 0;
-            for (int j = 0; j < left; j++) {
+            for (int j = 0; j < n; j++) {
                 if ((i & (1 << j)) != 0) {
                     sum += data[j];
                 }
             }
 
-            leftList.add(sum);
-        }
+            contains[sum] = true;
+        }//끝
 
-        //right
-        for (int i = 0; i < (1 << right); i++) {
-
-            int sum = 0;
-            for (int j = 0; j < right; j++) {
-                if ((i & (1 << j)) != 0) {
-                    sum += data[j+left];
-                }
-            }
-
-            rightList.add(sum);
-        }
-
-        Collections.sort(leftList);
-        Collections.sort(rightList);
-
-        int leftIndex = 0;
-        int rightIndex = rightList.size() - 1;
-        int lSize = leftList.size() - 1;
-
-        long answer = 0;
-        while (leftIndex <= lSize && rightIndex >= 0) {
-            int leftValue = leftList.get(leftIndex);
-            int rightValue = rightList.get(rightIndex);
-            long sum = leftValue + rightValue;
-
-            if (sum > s) {
-                rightIndex--;
-            }
-            else if(sum<s){
-                leftIndex++;
-            }
-            else{
-                long leftCheck = 0;
-                long rightCheck = 0;
-
-                while (leftIndex <= lSize && leftValue == leftList.get(leftIndex)) {
-                    leftIndex++;
-                    leftCheck++;
-                }
-
-                while (rightIndex >= 0 && rightValue == rightList.get(rightIndex)) {
-                    rightIndex--;
-                    rightCheck++;
-                }
-
-                answer += leftCheck * rightCheck;
+        //출력
+        for (int i = 1; i < contains.length; i++) {
+            if (!contains[i]) {
+                System.out.println(i);
+                break;
             }
         }
-        if(s==0) answer--;
-        System.out.println(answer);
-    }
 
-    /***************************************************************
-     * Programmers
-     **************************************************************/
-    public static class Programmers {
-
-        public int solution(int[] nums) {
-            int answer = -1;
-
-            // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-            System.out.println("Hello Java");
-
-            return answer;
-        }
-        // 이 위부터
     }
 }
+
