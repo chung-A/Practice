55b074b0bdee006df41b19bbe932449ae574927c
chung-A <ydh9517@naver.com>
2021-05-27 15:24:59 +0900

    백 1806번 부분합(투포인터)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -1,11 +1,6 @@
 package com.chung;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.StringTokenizer;
+import java.io.*;
 
 public class Main {
 
@@ -21,29 +16,42 @@ public class Main {
             data[i] = Integer.parseInt(split[i]);
         }
 
-        int answer = 0;
-        int startIdx = 0;
-        int endIdx = 0;
-        while (endIdx<=n) {
-
-            long sum = 0;
-            for (int i = startIdx; i < endIdx; i++) {
-                sum += data[i];
-            }
-
-            if (sum == m) {
-                answer++;
-                startIdx++;
-            }
-            else if (sum < m) {
-                endIdx++;
-            }
-            else{
-                startIdx++;
-            }
-        }
-
+        int answer = go(0, 0, 0, 0, m, 0, data);
         System.out.println(answer);
+    }
+
+    static int go(int prevStart, int prevEnd, int startIdx, int endIdx, int m, long prevSum,int[] data) {
+        if (endIdx <= data.length) {
+            long sum = 0;
+            //이전에 start++ 로 들어온 경우
+            if (prevStart < startIdx) {
+                sum = prevSum - data[prevStart];
+            }
+            //이전에 end++ 로 들어온 경우
+            else if (prevEnd < endIdx) {
+                sum = prevSum + data[endIdx - 1];
+            }
+
+            int answer = 0;
+            if (sum >= m) {
+                answer = go(startIdx, endIdx, startIdx + 1, endIdx, m, sum, data);
+
+                //길이 갱신
+                if (answer != 0) {
+                    answer = Math.min(answer, endIdx - startIdx);
+                } else {
+                    answer = endIdx - startIdx;
+                }
+
+            } else {
+                answer = go(startIdx, endIdx, startIdx, endIdx + 1, m, sum, data);
+            }
+
+            return answer;
+        }
+        else{
+            return 0;
+        }
     }
 }
 
