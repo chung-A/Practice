c2660e80c1c837a729a2b5321d0f472a2d41ff5f
chung-A <ydh9517@naver.com>
2021-05-28 15:27:50 +0900

    백 1316번 그룹단어 체커(문자열)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -1,62 +1,43 @@
 package com.chung;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
 
-    static boolean[] isNotSosu = new boolean[4000001];
-    static List<Integer> sosuList = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        getSosuList();
-
-        int startIdx = 0;
-        int endIdx = 0;
         int answer = 0;
-        while (endIdx <= sosuList.size()) {
+        for (int k = 0; k < n; k++) {
+            List<Character> wordList = new ArrayList<>();
 
-            long sum = 0;
-            for (int i = startIdx; i < endIdx; i++) {
-                sum += sosuList.get(i);
+            String word = br.readLine();
+            char prevChar = ' ';
+            boolean isGroupWord = true;
+            for (int i = 0; i < word.length(); i++) {
+                char c = word.charAt(i);
+
+                if (prevChar != c && wordList.contains(c)) {
+                    isGroupWord = false;
+                    break;
+                }
+                else if (prevChar != c) {
+                    prevChar = c;
+                    wordList.add(c);
+                }
             }
 
-            if (sum == n) {
+            if (isGroupWord) {
                 answer++;
-                startIdx++;
-            }
-            else if (sum < n) {
-                endIdx++;
-            }
-            else{
-                startIdx++;
             }
         }
-
         System.out.println(answer);
     }
-
-    static void getSosuList() {
-        isNotSosu[0] = true;
-        isNotSosu[1] = true;
-
-        for (int i = 2; i <= 4000000; i++) {
-            if (isNotSosu[i]) continue;
-
-            sosuList.add(i);
-
-            int multiply = 2;
-            while (i * multiply <= 4000000) {
-                int value = i * multiply;
-                isNotSosu[value] = true;
-                multiply++;
-            }
-        }
-    }
-
 }
 
