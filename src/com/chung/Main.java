0c59fcf20e122999d37fb4509b25e0c487ec78bc
chung-A <ydh9517@naver.com>
2021-05-28 15:41:50 +0900

    백 11650번 좌표 정렬하기(정렬)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,8 +3,7 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class Main {
 
@@ -12,32 +11,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
+        Map<Integer, List<Integer>> map = new HashMap<>();
 
-        int answer = 0;
-        for (int k = 0; k < n; k++) {
-            List<Character> wordList = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            String[] split = br.readLine().split(" ");
+            int x = Integer.parseInt(split[0]);
+            int y = Integer.parseInt(split[1]);
 
-            String word = br.readLine();
-            char prevChar = ' ';
-            boolean isGroupWord = true;
-            for (int i = 0; i < word.length(); i++) {
-                char c = word.charAt(i);
-
-                if (prevChar != c && wordList.contains(c)) {
-                    isGroupWord = false;
-                    break;
-                }
-                else if (prevChar != c) {
-                    prevChar = c;
-                    wordList.add(c);
-                }
+            if (map.containsKey(x)) {
+                List<Integer> list = map.get(x);
+                list.add(y);
             }
-
-            if (isGroupWord) {
-                answer++;
+            else {
+                List<Integer> list = new ArrayList<>();
+                list.add(y);
+                map.put(x, list);
             }
         }
-        System.out.println(answer);
+
+        map.keySet().stream().sorted().forEach(x-> {
+            List<Integer> list = map.get(x);
+            list.stream().sorted().forEach(y ->
+                    System.out.println(x + " " + y)
+            );
+        });
     }
 }
 
