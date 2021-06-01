16f36bde533092854a9b3987a9e0d8f72c7dd5a5
chung-A <ydh9517@naver.com>
2021-06-01 18:46:54 +0900

    백 2231번 분해합(브루트포스)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -4,47 +4,32 @@ import java.io.*;
 
 public class Main {
 
-    static int[] answer = new int[1000001];
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
 
-        answer[0] = 0;
-        answer[1] = 0;
-        answer[2] = 1;
-        answer[3] = 1;
-        int min = go(n);
+        int m = 1;
+        while (true) {
 
-        System.out.println(min);
+            String s = Integer.toString(m);
+
+            int sum = m;
+            for (int i = 0; i < s.length(); i++) {
+                sum += Integer.parseInt(String.valueOf(s.charAt(i)));
+            }
+
+            if (sum == n) {
+                break;
+            }
+            else if(m==1000000){
+                m = 0;
+                break;
+            }
+
+            m++;
+        }
+
+        System.out.println(m);
     }
 
-    static int go(int n) {
-        if (answer[n] != 0 || n == 1) {
-            return answer[n];
-        }
-
-        int min1 = 999999999;
-        int min2 = 999999999;
-        int min3 = 999999999;
-
-        if (n % 3 == 0) {
-            min1 = go(n / 3) + 1;
-        }
-
-        if (n % 2 == 0) {
-            min2 = go(n / 2) + 1;
-        }
-
-        min3 = go(n - 1) + 1;
-
-        //memoization
-        int temp = Math.min(min1, min2);
-        int min = Math.min(temp, min3);
-        answer[n] = min;
-
-        return min;
-    }
 }
-
-
