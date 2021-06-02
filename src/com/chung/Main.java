48f70bc3a58774c664ea3f99d1ec51d969a825d3
chung-A <ydh9517@naver.com>
2021-06-02 23:36:59 +0900

    백 11724번 연결 요소의 개수(그래프 탐색)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -1,35 +1,59 @@
 package com.chung;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        String[] sp = br.readLine().split(" ");
+        int n = Integer.parseInt(sp[0]);
+        int m = Integer.parseInt(sp[1]);
 
-        int[][] data = new int[n][2];
-        for (int i = 0; i < n; i++) {
-            String[] split = br.readLine().split(" ");
-            data[i][0] = Integer.parseInt(split[0]);
-            data[i][1] = Integer.parseInt(split[1]);
+        List<List<Integer>> data = new ArrayList<>();
+        for (int i = 0; i < n + 1; i++) {
+            List<Integer> list = new ArrayList<>();
+            data.add(list);
         }
 
-        StringBuilder stb = new StringBuilder();
-        for (int i = 0; i < n; i++) {
-            int rank = 1;
-            for (int j = 0; j < n; j++) {
-                if (i == j) {
-                    continue;
-                }
+        for (int i = 0; i < m; i++) {
+            sp = br.readLine().split(" ");
+            int from = Integer.parseInt(sp[0]);
+            int to = Integer.parseInt(sp[1]);
 
-                if (data[i][0] < data[j][0] && data[i][1] < data[j][1]) {
-                    rank++;
-                }
+            data.get(from).add(to);
+            data.get(to).add(from);
+        }
+
+        int answer = 0;
+        boolean[] visited = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) {
+            if (visited[i]) {
+                continue;
             }
 
-            stb.append(rank).append(" ");
+            //방문하지 않았으면 탐색 시작
+            answer++;
+            Queue<Integer> queue = new LinkedList<>();
+            queue.add(i);
+            while (!queue.isEmpty()) {
+                Integer node = queue.remove();
+                List<Integer> linkedNodes = data.get(node);
+                for (int j = 0; j < linkedNodes.size(); j++) {
+                    Integer linkedNode = linkedNodes.get(j);
+                    if (!visited[linkedNode]) {
+                        visited[linkedNode] = true;
+                        queue.add(linkedNode);
+                    }
+                }
+            }
         }
-        System.out.println(stb);
+        System.out.println(answer);
     }
 }
