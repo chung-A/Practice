f75144d5a6544806f1688f6e5f2d8a4710f7be15
chung-A <ydh9517@naver.com>
2021-06-03 00:56:49 +0900

    백 2667번 단지번호붙이기(연결요소, dfs)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -1,59 +1,58 @@
 package com.chung;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.io.*;
+import java.util.*;
 
 public class Main {
 
+    //상하좌우
+    static final int[] dx = new int[]{0, 0, -1, 1};
+    static final int[] dy = new int[]{1, -1, 0, 0};
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] sp = br.readLine().split(" ");
-        int n = Integer.parseInt(sp[0]);
-        int m = Integer.parseInt(sp[1]);
+        int n = Integer.parseInt(br.readLine());
 
-        List<List<Integer>> data = new ArrayList<>();
-        for (int i = 0; i < n + 1; i++) {
-            List<Integer> list = new ArrayList<>();
-            data.add(list);
-        }
-
-        for (int i = 0; i < m; i++) {
-            sp = br.readLine().split(" ");
-            int from = Integer.parseInt(sp[0]);
-            int to = Integer.parseInt(sp[1]);
-
-            data.get(from).add(to);
-            data.get(to).add(from);
-        }
-
-        int answer = 0;
-        boolean[] visited = new boolean[n + 1];
-        for (int i = 1; i <= n; i++) {
-            if (visited[i]) {
-                continue;
+        int[][] map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = str.charAt(j) - '0';
             }
+        }
 
-            //방문하지 않았으면 탐색 시작
-            answer++;
-            Queue<Integer> queue = new LinkedList<>();
-            queue.add(i);
-            while (!queue.isEmpty()) {
-                Integer node = queue.remove();
-                List<Integer> linkedNodes = data.get(node);
-                for (int j = 0; j < linkedNodes.size(); j++) {
-                    Integer linkedNode = linkedNodes.get(j);
-                    if (!visited[linkedNode]) {
-                        visited[linkedNode] = true;
-                        queue.add(linkedNode);
-                    }
+        int count = 0;
+        List<Integer> answer = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (map[i][j] > 0) {
+                    //해당 점 탐색시작
+                    count++;
+                    answer.add(dfs(i, j, map));
                 }
             }
         }
-        System.out.println(answer);
+
+        System.out.println(count);
+        Collections.sort(answer);
+        for (Integer integer : answer) {
+            System.out.println(integer);
+        }
+    }
+
+    static int dfs(int x, int y,int[][] map) {
+        map[x][y] = 0;
+        int count = 1;
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+
+            if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length) {
+                if(map[nx][ny]>0){
+                    count += dfs(nx, ny, map);
+                }
+            }
+        }
+        return count;
     }
 }
