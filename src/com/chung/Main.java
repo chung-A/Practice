25f4f60d534c06e1fb8f7c86fa61a82a8c99d9d2
chung-A <ydh9517@naver.com>
2021-06-03 15:06:23 +0900

    Java multithread 실습

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -5,67 +5,78 @@ import java.util.*;
 
 public class Main {
 
-    public static class Location{
-        int x;
-        int y;
+    public static class MyThread1 extends Thread {
 
-        public Location(int x, int y) {
-            this.x = x;
-            this.y = y;
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println("hello thread1!");
+                    Thread.sleep(5000);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("i am interrupted!");
+            }
+        }
+
+    }
+
+    public static class MyThread2 implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                while (true) {
+                    System.out.println("hello thread2!");
+                    Thread.sleep(1000);
+                }
+            } catch (InterruptedException e) {
+                System.out.println("i am interrupted!");
+            }
         }
     }
 
-    //상하좌우
-    static final int[] dx = new int[]{0, 0, -1, 1};
-    static final int[] dy = new int[]{1, -1, 0, 0};
+    public static void main(String[] args) throws IOException, InterruptedException {
+        //상속받는 방법
+        MyThread1 thread1 = new MyThread1();
+//        thread1.start();
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = br.readLine().split(" ");
-        int n = Integer.parseInt(split[0]);
-        int m = Integer.parseInt(split[1]);
+        //runnable 구현하는 방법
+        Thread thread2 = new Thread(new MyThread2());
+//        thread2.start();
 
-        int[][] map = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < m; j++) {
-                map[i][j] = str.charAt(j) - '0';
-            }
-        }
-
-        boolean[][] visited = new boolean[n][m];
-        Queue<Location> queue = new LinkedList<>();
-        queue.add(new Location(0, 0));
-        visited[0][0] = true;
-
-        int depth = 1;
-        while (!queue.isEmpty()) {
-            int length = queue.size();
-            for (int k = 0; k < length; k++) {
-                Location node = queue.remove();
-
-                //도착시 중지
-                if (node.x == map.length - 1 && node.y == map[0].length - 1) {
-                    System.out.println(depth);
-                    return;
+        //람다식 이용하기
+        Runnable task = () -> {
+            try {
+                int i = 0;
+                while (i < 5) {
+                    i++;
+                    System.out.println("hello thread3!");
+                    Thread.sleep(1000);
                 }
-
-                for (int i = 0; i < 4; i++) {
-                    int nx = node.x + dx[i];
-                    int ny = node.y + dy[i];
-
-                    //멥 안에 있고
-                    if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length) {
-                        //길이 있으면
-                        if (map[nx][ny] == 1 && !visited[nx][ny]) {
-                            queue.add(new Location(nx, ny));
-                            visited[nx][ny] = true;
-                        }
-                    }
-                }
-
+            } catch (InterruptedException e) {
+                System.out.println("i am interrupted!");
             }
-            depth++;
+        };
+
+        Thread thread3 = new Thread(task);
+//        thread3.start();
+
+/*
+        try {
+            System.out.println("parent is wait you!!");
+            thread3.join(); //부모쓰레드는 여기서 대기
+        } catch (InterruptedException e) {
+            System.out.println("parent interrupted");
         }
+*/
+
+        thread2.start();
+        Thread.sleep(5000);
+        thread2.interrupt();
+
+        System.out.println("hello my child!");
+
+
     }
 }
