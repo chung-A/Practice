802ab6aa0e2a3a20cebffb925e1fdaac787a38d7
chung-A <ydh9517@naver.com>
2021-06-03 10:41:08 +0900

    백 2178번 미로찾기(bfs+depth 구해보기)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -5,42 +5,7 @@ import java.util.*;
 
 public class Main {
 
-    //상하좌우
-    static final int[] dx = new int[]{0, 0, -1, 1};
-    static final int[] dy = new int[]{1, -1, 0, 0};
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        int[][] map = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < n; j++) {
-                map[i][j] = str.charAt(j) - '0';
-            }
-        }
-
-        int count = 0;
-        List<Integer> answer = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (map[i][j] > 0) {
-                    //해당 점 탐색시작
-                    count++;
-                    answer.add(bfs(new Location(i, j), map));
-                }
-            }
-        }
-
-        System.out.println(count);
-        Collections.sort(answer);
-        for (Integer integer : answer) {
-            System.out.println(integer);
-        }
-    }
-
-    static class Location{
+    public static class Location{
         int x;
         int y;
 
@@ -50,40 +15,57 @@ public class Main {
         }
     }
 
-    static int bfs(Location start,int[][] map) {
-        int danjiCount = 0;
-        map[start.x][start.y] = 0;
+    //상하좌우
+    static final int[] dx = new int[]{0, 0, -1, 1};
+    static final int[] dy = new int[]{1, -1, 0, 0};
 
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
+
+        int[][] map = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        boolean[][] visited = new boolean[n][m];
         Queue<Location> queue = new LinkedList<>();
-        queue.add(start);
-        int depth = 0;
+        queue.add(new Location(0, 0));
+        visited[0][0] = true;
+
+        int depth = 1;
         while (!queue.isEmpty()) {
-            danjiCount++;
-
             int length = queue.size();
-            System.out.println("---------------------------------------");
-            System.out.println("depth: " + depth);
-            System.out.println("---------------------------------------");
-
             for (int k = 0; k < length; k++) {
                 Location node = queue.remove();
-                System.out.println("x: " + node.x + "/y: " + node.y);
+
+                //도착시 중지
+                if (node.x == map.length - 1 && node.y == map[0].length - 1) {
+                    System.out.println(depth);
+                    return;
+                }
 
                 for (int i = 0; i < 4; i++) {
                     int nx = node.x + dx[i];
                     int ny = node.y + dy[i];
 
-                    if(nx>=0&&nx<map.length&&ny>=0&&ny<map[0].length) {
-                        if (map[nx][ny] > 0) {
-                            map[nx][ny] = 0;
+                    //멥 안에 있고
+                    if (nx >= 0 && nx < map.length && ny >= 0 && ny < map[0].length) {
+                        //길이 있으면
+                        if (map[nx][ny] == 1 && !visited[nx][ny]) {
                             queue.add(new Location(nx, ny));
+                            visited[nx][ny] = true;
                         }
                     }
                 }
+
             }
             depth++;
         }
-        System.out.println();
-        return danjiCount;
     }
 }
