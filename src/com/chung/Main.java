4bfb1065401cd4d7680bb83ee35de6b070ee9105
chung-A <ydh9517@naver.com>
2021-06-03 18:59:15 +0900

    백 7569번 토마토(bfs)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -5,78 +5,106 @@ import java.util.*;
 
 public class Main {
 
-    public static class MyThread1 extends Thread {
+    //동서남북위아래
+    static final int[] dx = new int[]{0, 0, 1, -1, 0, 0};
+    static final int[] dy = new int[]{1, -1, 0, 0, 0, 0};
+    static final int[] dz = new int[]{0, 0, 0, 0, 1, -1};
 
-        @Override
-        public void run() {
-            try {
-                while (true) {
-                    System.out.println("hello thread1!");
-                    Thread.sleep(5000);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        int m = Integer.parseInt(split[0]);
+        int n = Integer.parseInt(split[1]);
+        int h = Integer.parseInt(split[2]);
+
+        Queue<Location> q = new LinkedList<>();
+        boolean allFinish = true;
+        int[][][] box = new int[n][m][h];
+        for (int i = 0; i < h; i++) {
+            for (int j = 0; j < n; j++) {
+                split = br.readLine().split(" ");
+                for (int k = 0; k < m; k++) {
+                    int value = Integer.parseInt(split[k]);
+                    box[j][k][i] = value;
+
+                    if (value == 1) {
+                        q.add(new Location(j, k, i));
+                    }
+
+                    if (allFinish && value == 0) {
+                        allFinish = false;
+                    }
                 }
-            } catch (InterruptedException e) {
-                System.out.println("i am interrupted!");
             }
         }
 
+        if (allFinish) {
+            System.out.println(0);
+            return;
+        }
+
+        int[][][] dist = new int[box.length][box[0].length][box[0][0].length];
+        bfs(q, dist, box);
+
+        int answer = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                for (int k = 0; k < h; k++) {
+                    if (box[i][j][k] == 0 && dist[i][j][k] == 0) {
+                        System.out.println(-1);
+                        return;
+                    }
+
+                    if (box[i][j][k] == 0 && dist[i][j][k] > 0) {
+                        answer = Math.max(dist[i][j][k], answer);
+                    }
+
+                }
+            }
+        }
+
+        System.out.println(answer);
     }
 
-    public static class MyThread2 implements Runnable {
+    static void bfs(Queue<Location> queue, int[][][] dist, int[][][] box) {
+        while (!queue.isEmpty()) {
+            Location node = queue.remove();
 
-        @Override
-        public void run() {
-            try {
-                while (true) {
-                    System.out.println("hello thread2!");
-                    Thread.sleep(1000);
+            for (int k = 0; k < 6; k++) {
+                int nx = node.x + dx[k];
+                int ny = node.y + dy[k];
+                int nz = node.z + dz[k];
+
+                //범위내에 있고
+                if (nx >= 0 && nx < box.length &&
+                        ny >= 0 && ny < box[0].length &&
+                        nz >= 0 && nz < box[0][0].length) {
+                    int nextPointValue = box[nx][ny][nz];
+
+                    if (nextPointValue == 0&&dist[nx][ny][nz]==0) {
+                        if (dist[nx][ny][nz] == 0) {
+                            dist[nx][ny][nz] = dist[node.x][node.y][node.z] + 1;
+                        }
+                        else{
+                            dist[nx][ny][nz] = Math.min(dist[node.x][node.y][node.z] + 1, dist[nx][ny][nz]);
+                        }
+
+                        queue.add(new Location(nx, ny, nz));
+                    }
                 }
-            } catch (InterruptedException e) {
-                System.out.println("i am interrupted!");
             }
         }
     }
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-        //상속받는 방법
-        MyThread1 thread1 = new MyThread1();
-//        thread1.start();
+    static class Location {
+        int x;
+        int y;
+        int z;
 
-        //runnable 구현하는 방법
-        Thread thread2 = new Thread(new MyThread2());
-//        thread2.start();
-
-        //람다식 이용하기
-        Runnable task = () -> {
-            try {
-                int i = 0;
-                while (i < 5) {
-                    i++;
-                    System.out.println("hello thread3!");
-                    Thread.sleep(1000);
-                }
-            } catch (InterruptedException e) {
-                System.out.println("i am interrupted!");
-            }
-        };
-
-        Thread thread3 = new Thread(task);
-//        thread3.start();
-
-/*
-        try {
-            System.out.println("parent is wait you!!");
-            thread3.join(); //부모쓰레드는 여기서 대기
-        } catch (InterruptedException e) {
-            System.out.println("parent interrupted");
+        public Location(int x, int y, int z) {
+            this.x = x;
+            this.y = y;
+            this.z = z;
         }
-*/
-
-        thread2.start();
-        Thread.sleep(5000);
-        thread2.interrupt();
-
-        System.out.println("hello my child!");
-
-
     }
 }
