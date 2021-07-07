e77eaf6f312ba82388c7d635b8bbd22afde29a75
chung-A <ydh9517@naver.com>
2021-07-07 12:06:03 +0900

    백 2775번 부녀회장이 될테야(수학)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -1,21 +1,38 @@
 package com.chung;
 
-import com.chung.study.MyHashTable;
-
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
-    static final long MOD = 1000000009;
 
     public static void main(String[] args) throws IOException {
-/*        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        long[][] d = new long[limit + 1][3];*/
-        int[] d = new int[]{70,50,  80, 50};
-//        System.out.println(solution(d,100));
+
+        while (t > 0) {
+            int k = Integer.parseInt(br.readLine());
+            int n = Integer.parseInt(br.readLine());
+
+            int[][] data = new int[k + 1][n + 1];
+            for (int i = 0; i < data.length; i++) {
+                for (int j = 0; j < data[0].length; j++) {
+                    if (i == 0) {
+                        data[i][j] = j;
+                    }else{
+                        if (j == 0) {
+                            data[i][j] = data[i - 1][j];
+                        }else{
+                            data[i][j] = data[i - 1][j] + data[i][j - 1];
+                        }
+                    }
+                }
+            }
+            System.out.println(data[k][n]);
+            t--;
+        }
     }
 
     public static int solution(int[] people, int limit) {
@@ -24,7 +41,7 @@ public class Main {
 
         int startIdx = 0;
 
-        for (int max = people.length-1; startIdx <= max; max--) {
+        for (int max = people.length - 1; startIdx <= max; max--) {
             if (people[startIdx] + people[max] <= limit) {
                 startIdx++;
             }
--- src/com/chung/study/MyHashTable.java
+++ src/com/chung/study/MyHashTable.java
@@ -1,5 +1,7 @@
 package com.chung.study;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 
