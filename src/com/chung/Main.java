328e615e4ba6e0debda5f596924e9998ce263035
chung-A <ydh9517@naver.com>
2021-07-08 00:18:19 +0900

    백 15740번 A+B - 9(수학)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -3,6 +3,7 @@ package com.chung;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.math.BigInteger;
 import java.util.Arrays;
 
 public class Main {
@@ -10,29 +11,11 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        String[] split = br.readLine().split(" ");
+        BigInteger a = new BigInteger(split[0]);
+        BigInteger b = new BigInteger(split[1]);
 
-        while (t > 0) {
-            int k = Integer.parseInt(br.readLine());
-            int n = Integer.parseInt(br.readLine());
-
-            int[][] data = new int[k + 1][n + 1];
-            for (int i = 0; i < data.length; i++) {
-                for (int j = 0; j < data[0].length; j++) {
-                    if (i == 0) {
-                        data[i][j] = j;
-                    }else{
-                        if (j == 0) {
-                            data[i][j] = data[i - 1][j];
-                        }else{
-                            data[i][j] = data[i - 1][j] + data[i][j - 1];
-                        }
-                    }
-                }
-            }
-            System.out.println(data[k][n]);
-            t--;
-        }
+        System.out.println(a.add(b));
     }
 
     public static int solution(int[] people, int limit) {
