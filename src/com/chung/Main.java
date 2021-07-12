f45ea55d72edfbea464ae89cc4f69a48e1be3da2
chung-A <ydh9517@naver.com>
2021-07-12 11:31:55 +0900

    백 2805번 나무자르기(구현)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -7,60 +7,36 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
+        String[] split = br.readLine().split(" ");
+        int n = Integer.parseInt(split[0]);
+        int m = Integer.parseInt(split[1]);
 
-        List<Location> list = new ArrayList<>();
-        while (t > 0) {
-            t--;
-            String[] split = br.readLine().split(" ");
-
-            int x = Integer.parseInt(split[0]);
-            int y = Integer.parseInt(split[1]);
-
-            list.add(new Location(x, y));
+        split = br.readLine().split(" ");
+        int[] trees = new int[n];
+        int height = 0;
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            trees[i] = Integer.parseInt(split[i]);
+            if (trees[i] > height) {
+                height = trees[i];
+            }
         }
 
-        list.sort((l1,l2)->{
-            if(l1.getY()==l2.getY()){
-                if (l1.getX() > l2.getX()) {
-                    return 1;
-                }
-                else{
-                    return -1;
-                }
-            }
-            else{
-                if (l1.getY() > l2.getY()) {
-                    return 1;
-                }
-                else{
-                    return -1;
-                }
-            }
-        });
+        Arrays.sort(trees);
+        while (sum < m) {
+            height--;
 
-        for (Location location : list) {
-            System.out.println(location.getX()+" "+location.getY());
+            for (int i = trees.length - 1; i >= 0; i--) {
+                if (trees[i] > height) {
+                    sum ++;
+                    continue;
+                }
+                break;
+            }
         }
+        System.out.println(height);
     }
 
-    public static class Location{
-        int x;
-        int y;
-
-        public Location(int x, int y) {
-            this.x = x;
-            this.y = y;
-        }
-
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
-    }
     public static int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
