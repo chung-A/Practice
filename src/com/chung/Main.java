a97a4947c7da3038ba91a151fa904c122806f9c9
chung-A <ydh9517@naver.com>
2021-07-13 11:51:34 +0900

    백 2110번 공유기 설치(이분탐색)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -7,51 +7,60 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(br.readLine());
         String[] split = br.readLine().split(" ");
-        int[] cards = new int[n];
+        int n = Integer.parseInt(split[0]);
+        int c = Integer.parseInt(split[1]);
+
+        int[] homes = new int[n];
         for (int i = 0; i < n; i++) {
-            cards[i] = Integer.parseInt(split[i]);
+            homes[i] = Integer.parseInt(br.readLine());
         }
 
-        Arrays.sort(cards);
+        Arrays.sort(homes);
 
-        StringBuilder stb = new StringBuilder();
-        int m = Integer.parseInt(br.readLine());
-        split = br.readLine().split(" ");
-        for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(split[i]);
+        long left = 0;
+        long right = homes[homes.length - 1];
+        long answer = right;
+        while (left <= right) {
+            //가장 인접한 공유기 사이의 최소 거리
+            long mid = (left + right) / 2;
 
-            //이분탐색
-            int left=0;
-            int right = cards.length-1;
+            //찾은경우 - 더 큰 값을 찾아본다.
+            if (check(homes, c, mid)) {
+                answer = mid;
+                left = mid + 1;
+            }
+            //못찾은 경우-공유기사이의 거리를 더 줄여본다.
+            else{
+                right = mid - 1;
+            }
+        }
+        System.out.println(answer);
+    }
+
+    public static boolean check(int[] homes,int c, long dist) {
+        int pos = homes[0];
+        int idx = 1;
+        c--;
+
+        while (c>0) {
+            c--;
+            long nextPos =pos + dist;
+
             boolean findYn = false;
-            while (left <= right) {
-                int mid = (left + right) / 2;
-                int card = cards[mid];
-
-                if (card == num) {
+            for (;idx < homes.length; idx++) {
+                if (homes[idx] >= nextPos) {
+                    pos = homes[idx];
                     findYn = true;
                     break;
                 }
-                else if (card < num) {
-                    left = mid + 1;
-                }
-                else{
-                    right = mid - 1;
-                }
             }
 
-            if (findYn) {
-                stb.append("1 ");
-            }
-            else{
-                stb.append("0 ");
+            if (!findYn) {
+                return false;
             }
         }
-
-        System.out.println(stb);
+        return true;
     }
 
     public static long solution(int n, int[] times) {
