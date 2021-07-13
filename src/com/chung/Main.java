0471290f733e19d007b3e3cdb11725067ed13874
chung-A <ydh9517@naver.com>
2021-07-13 11:04:16 +0900

    백 10815번 숫자카드(이분탐색)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -6,11 +6,52 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-/*        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = br.readLine().split(" ");*/
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        long solution = solution(6, new int[]{7, 10});
-        System.out.println(solution);
+        int n = Integer.parseInt(br.readLine());
+        String[] split = br.readLine().split(" ");
+        int[] cards = new int[n];
+        for (int i = 0; i < n; i++) {
+            cards[i] = Integer.parseInt(split[i]);
+        }
+
+        Arrays.sort(cards);
+
+        StringBuilder stb = new StringBuilder();
+        int m = Integer.parseInt(br.readLine());
+        split = br.readLine().split(" ");
+        for (int i = 0; i < m; i++) {
+            int num = Integer.parseInt(split[i]);
+
+            //이분탐색
+            int left=0;
+            int right = cards.length-1;
+            boolean findYn = false;
+            while (left <= right) {
+                int mid = (left + right) / 2;
+                int card = cards[mid];
+
+                if (card == num) {
+                    findYn = true;
+                    break;
+                }
+                else if (card < num) {
+                    left = mid + 1;
+                }
+                else{
+                    right = mid - 1;
+                }
+            }
+
+            if (findYn) {
+                stb.append("1 ");
+            }
+            else{
+                stb.append("0 ");
+            }
+        }
+
+        System.out.println(stb);
     }
 
     public static long solution(int n, int[] times) {
