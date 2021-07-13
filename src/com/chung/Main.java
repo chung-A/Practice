3fb0eeb4f5126fffb760142d8fae2a93fd566711
chung-A <ydh9517@naver.com>
2021-07-13 12:40:02 +0900

    programmers 예산

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -6,83 +6,35 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = br.readLine().split(" ");
-        int n = Integer.parseInt(split[0]);
-        int c = Integer.parseInt(split[1]);
-
-        int[] homes = new int[n];
-        for (int i = 0; i < n; i++) {
-            homes[i] = Integer.parseInt(br.readLine());
-        }
-
-        Arrays.sort(homes);
-
-        long left = 0;
-        long right = homes[homes.length - 1];
-        long answer = right;
-        while (left <= right) {
-            //가장 인접한 공유기 사이의 최소 거리
-            long mid = (left + right) / 2;
-
-            //찾은경우 - 더 큰 값을 찾아본다.
-            if (check(homes, c, mid)) {
-                answer = mid;
-                left = mid + 1;
-            }
-            //못찾은 경우-공유기사이의 거리를 더 줄여본다.
-            else{
-                right = mid - 1;
-            }
-        }
-        System.out.println(answer);
+/*        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");*/
+        int solution = solution(new int[]{2,2,3,3}, 10);
+        System.out.println("solution = " + solution);
     }
 
-    public static boolean check(int[] homes,int c, long dist) {
-        int pos = homes[0];
-        int idx = 1;
-        c--;
+    public static int solution(int[] d, int budget) {
+        int answer = 0;
+        Arrays.sort(d);
 
-        while (c>0) {
-            c--;
-            long nextPos =pos + dist;
-
-            boolean findYn = false;
-            for (;idx < homes.length; idx++) {
-                if (homes[idx] >= nextPos) {
-                    pos = homes[idx];
-                    findYn = true;
-                    break;
-                }
-            }
-
-            if (!findYn) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    public static long solution(int n, int[] times) {
-        long answer = 0;
-
-        Arrays.sort(times);
-        long left = 0;
-        long right = (long) times[times.length - 1] * n;
+        //Mid: 물품지원한 부서 갯수
+        int left = 0;
+        int right = d.length;
 
         while (left <= right) {
-            long mid = (left + right) / 2;   //추정 시간
-            long sum = 0;   //추정시간 시 심사볼 수 있는 사람 합계
+            int mid = (left + right) / 2;
 
-            for (int i = 0; i < times.length; i++) {
-                sum += mid / times[i];
+            long sum = 0;
+            for (int i = 0; i < mid; i++) {
+                sum += d[i];
             }
 
-            if (sum < n) {
+            //늘려보기
+            if (sum <= budget) {
                 left = mid + 1;
-            }
-            else{
                 answer = mid;
+            }
+            //줄여보기
+            else{
                 right = mid - 1;
             }
         }
