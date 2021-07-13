16e13727c2a9eca1a145a6893d52693db061c766
chung-A <ydh9517@naver.com>
2021-07-13 10:39:55 +0900

    프로그래머스 입국심사(이분탐색)

--- src/com/chung/Main.java
+++ src/com/chung/Main.java
@@ -6,50 +6,36 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] split = br.readLine().split(" ");
-        int n = Integer.parseInt(split[0]);
-        int m = Integer.parseInt(split[1]);
+/*        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");*/
 
-        split = br.readLine().split(" ");
-        int[] trees = new int[n];
-        int height = 0;
-        long sum = 0;
-        for (int i = 0; i < n; i++) {
-            trees[i] = Integer.parseInt(split[i]);
-            if (trees[i] > height) {
-                height = trees[i];
-            }
-        }
-
-        Arrays.sort(trees);
-        while (sum < m) {
-            height--;
-
-            for (int i = trees.length - 1; i >= 0; i--) {
-                if (trees[i] > height) {
-                    sum ++;
-                    continue;
-                }
-                break;
-            }
-        }
-        System.out.println(height);
+        long solution = solution(6, new int[]{7, 10});
+        System.out.println(solution);
     }
 
-    public static int solution(int[] people, int limit) {
-        int answer = 0;
-        Arrays.sort(people);
+    public static long solution(int n, int[] times) {
+        long answer = 0;
 
-        int startIdx = 0;
+        Arrays.sort(times);
+        long left = 0;
+        long right = (long) times[times.length - 1] * n;
 
-        for (int max = people.length - 1; startIdx <= max; max--) {
-            if (people[startIdx] + people[max] <= limit) {
-                startIdx++;
+        while (left <= right) {
+            long mid = (left + right) / 2;   //추정 시간
+            long sum = 0;   //추정시간 시 심사볼 수 있는 사람 합계
+
+            for (int i = 0; i < times.length; i++) {
+                sum += mid / times[i];
             }
-            answer++;
-        }
 
+            if (sum < n) {
+                left = mid + 1;
+            }
+            else{
+                answer = mid;
+                right = mid - 1;
+            }
+        }
         return answer;
     }
 }
